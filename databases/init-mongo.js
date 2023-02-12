b59c4b25cbb3a3810171f158bbff6c90ca5516cb
aravind <aravind.arali@gmail.com>
2023-02-12 21:30:58 +0530

    added dev dbs mysql + mongodb (bug fix for same db entries for both servers)

--- databases/init-mongo.js
+++ databases/init-mongo.js
@@ -1,28 +1,19 @@
-db = db.getSiblingDB('user_service_db');
+db = db.getSiblingDB('cart_service_db');
 db.createUser(
   {
     user: 'user',
     pwd: 'password',
-    roles: [{ role: 'readWrite', db: 'user_service_db' }],
+    roles: [{ role: 'readWrite', db: 'cart_service_db' }],
   },
 );
 
 
-db = db.getSiblingDB('product_service_db');
+db = db.getSiblingDB('order_service_db');
 db.createUser(
   {
     user: 'user',
     pwd: 'password',
-    roles: [{ role: 'readWrite', db: 'product_service_db' }],
+    roles: [{ role: 'readWrite', db: 'order_service_db' }],
   },
 );
 
-
-db = db.getSiblingDB('inventory_service_db');
-db.createUser(
-  {
-    user: 'user',
-    pwd: 'password',
-    roles: [{ role: 'readWrite', db: 'inventory_service_db' }],
-  },
-);
